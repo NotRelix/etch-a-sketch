887d0eee2f69355f4f6350b0fd58f8e26b52ce6a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-23 20:34:39 +0800

    feat: resize canvas on user input

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const canvas = document.querySelector('.canvas');
+const canvasSizeBtn = document.querySelector('button');
+
+let canvasSize = 16;
 
 function handleMouseOver(e) {
   e.target.classList.add('hovered');
 }
 
-for (let row = 0; row < 16; row++) {
-  const rowCanvas = document.createElement('div');
-  rowCanvas.setAttribute('class', 'row');
-  for (let col = 0; col < 16; col++) {
-    const colCanvas = document.createElement('div');
-    colCanvas.setAttribute('class', 'pixel');
-    rowCanvas.appendChild(colCanvas);
+function createCanvas(canvasSize) {
+  for (let row = 0; row < canvasSize; row++) {
+    const rowCanvas = document.createElement('div');
+    rowCanvas.setAttribute('class', 'row');
+    for (let col = 0; col < canvasSize; col++) {
+      const colCanvas = document.createElement('div');
+      colCanvas.setAttribute('class', 'pixel');
+      rowCanvas.appendChild(colCanvas);
+    }
+    canvas.appendChild(rowCanvas);
   }
-  canvas.appendChild(rowCanvas);
 }
+
+function deleteCanvas(canvasSize) {
+  for (let row = 0; row < canvasSize; row++) {
+    canvas.removeChild(canvas.firstChild)
+  }
+}
+
+createCanvas(canvasSize);
 
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach(pixel => {
   pixel.addEventListener('mouseover', handleMouseOver)
+})
+
+canvasSizeBtn.addEventListener('click', () => {
+  const newCanvasSize = +prompt('Enter canvas size');
+  if (newCanvasSize > 0 && newCanvasSize <= 100) {
+    deleteCanvas(canvasSize);
+    createCanvas(newCanvasSize);
+    canvasSize = newCanvasSize;
+  }
 })
